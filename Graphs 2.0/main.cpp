bdec3e7cecbadbd3dea92053bf6e6344b817a6c0
Krzysztof Sychla <ksychla5@gmail.com>
2018-05-17 09:53:49 +0200

    Initial Commit

---
+++ Graphs 2.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Graphs 2.0
+//
+//  Created by Krzysztof Sychla on 17.05.2018.
+//  Copyright © 2018 Krzysztof Sychla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
